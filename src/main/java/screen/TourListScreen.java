40c39b8cb2a6989fc19b5d2a6e14bf705838cfaf
gnud0505 <vandung0505nb@gmail.com>
2024-06-16 23:43:33 +0700

    update tourlist

--- src/main/java/screen/TourListScreen.java
+++ src/main/java/screen/TourListScreen.java
@@ -1,6 +1,6 @@
 package screen;
+
 import controller.SampleTourController;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -14,68 +14,37 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import model.SampleTour;
-import org.w3c.dom.events.Event;
 
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.*;
 
-public class TourScreen implements Initializable {
-
+public class TourListScreen implements Initializable {
     @FXML
-    private HBox popular1;
-
-    @FXML
-    private HBox popular2;
-
-    @FXML
-    private HBox sale1;
-
-    @FXML
-    private HBox sale2;
-
-    @FXML
-    private Button more1;
-
-    @FXML
-    private Button more2;
-
+    private VBox tourList;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         SampleTourController sampleTourController = new SampleTourController();
         try {
-            List<SampleTour> tour = sampleTourController.getAll();
-            List<SampleTour> newTours = new ArrayList<>();
-            for (int i = tour.size() - 1; i >= tour.size() - 6; i--) {
-                newTours.add(tour.get(i));
-            }
-            Collections.sort(tour, new Comparator<SampleTour>() {
+            List<SampleTour> tours = sampleTourController.getAll();
+            Collections.sort(tours, new Comparator<SampleTour>() {
                 @Override
                 public int compare(SampleTour o1, SampleTour o2) {
                     return Double.compare(o1.getTotalCost(), o2.getTotalCost());
                 }
             });
-            List<SampleTour> cheapestTour = new ArrayList<>();
-            for (int i = 0; i < 6; i++) {
-                cheapestTour.add(tour.get(i));
+
+            for(int i = 0; i < tours.size(); i=i+3){
+                HBox hBox = new HBox();
+                hBox.setSpacing(20);
+                hBox.setAlignment(Pos.BASELINE_LEFT);
+                for(int j =0; j<3 && i+j<tours.size(); j++){
+                    hBox.getChildren().add(createDestinationBox(tours.get(i+j)));
+                }
+                tourList.getChildren().add(hBox);
             }
 
-            for (int i = 0; i < 3; i++) {
-                popular1.getChildren().add(createDestinationBox(newTours.get(i)));
-            }
-            for (int i = 3; i < 6; i++) {
-                popular2.getChildren().add(createDestinationBox(newTours.get(i)));
-            }
-            for (int i = 0; i < 3; i++) {
-                sale1.getChildren().add(createDestinationBox(cheapestTour.get(i)));
-            }
-            for (int i = 3; i < 6; i++) {
-                sale2.getChildren().add(createDestinationBox(cheapestTour.get(i)));
-            }
-
-            more1.setOnAction(event -> changeTourList(event));
-            more2.setOnAction(event -> changeTourList(event));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -83,9 +52,16 @@ public class TourScreen implements Initializable {
 
     private VBox createDestinationBox(SampleTour tour) {
         VBox vBox = new VBox();
+        vBox.setPrefWidth(270);
         vBox.setAlignment(Pos.CENTER);
         vBox.setSpacing(10);
-        vBox.getStyleClass().add("destination-box");
+        vBox.setStyle("-fx-spacing: 16;\n" +
+                "    -fx-padding: 10;\n" +
+                "    -fx-border-width: 1;\n" +
+                "    -fx-border-color: lightgray;\n" +
+                "    -fx-border-radius: 10px;\n" +
+                "    -fx-background-color: #fff;\n" +
+                "    -fx-background-radius: 10px;");
 
         ImageView imageView = new ImageView(new Image(tour.getLocations().getFirst().getKey().getImageUrl()));
         imageView.setFitHeight(150);
@@ -99,7 +75,14 @@ public class TourScreen implements Initializable {
 
         Label nameLabel = new Label(tour.getTourName());
         nameLabel.setMaxWidth(160);
+        nameLabel.setStyle("-fx-font-size: 16px;\n" +
+                "    -fx-font-family: Arial;\n" +
+                "    -fx-font-weight: bold;");
         Label priceLabel = new Label(tour.getTotalCost() + " $");
+        priceLabel.setStyle("-fx-font-size: 16px;\n" +
+                "    -fx-font-family: Arial;\n" +
+                "    -fx-font-weight: bold;");
+
         Region region = new Region();
         HBox.setHgrow(region, Priority.ALWAYS);
 
@@ -120,24 +103,16 @@ public class TourScreen implements Initializable {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            BorderPane userView = (BorderPane) popular1.getScene().lookup("#userView");
+            BorderPane userView = (BorderPane) ((Node) event.getSource()).getScene().lookup("#userView");
             userView.setCenter(view);
         });
+        bookButton.setStyle("-fx-background-color: #fd6951;\n" +
+                "    -fx-text-fill: white;\n" +
+                "    -fx-background-radius: 16px;\n" +
+                "    -fx-font-weight: bold;");
 
         vBox.getChildren().addAll(imageView, hBox, descriptionText, bookButton);
 
         return vBox;
-    }
-
-    public void changeTourList(ActionEvent event){
-        ScrollPane view = null;
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/user/tourlist.fxml"));
-            view = loader.load();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        BorderPane userView = (BorderPane) ((Node) event.getSource()).getScene().lookup("#userView");
-        userView.setCenter(view);
     }
 }
